b060d2a291e472f2cca704d27d30c034bf2ab900
Josh <josh@twilio.com>
2018-10-02 10:09:05 -0700

    Cleaning up bulk lookup file.

--- bulkLookups.js
+++ bulkLookups.js
@@ -1,5 +1,5 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////
-////           Call script with: node bulkLookups.js SID Auth Phone Number .csv               ////
+////           Call script with: node bulkLookups.js input-file.csv               ////
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
 var sid = process.env.BULK_SID;
@@ -8,10 +8,10 @@ var lookupType = process.env.BULK_LOOKUP_TYPE; // fraud or carrier
 var bulkCSVHeaders = process.env.BULK_CSV_HEADERS;
 var errorOutput = process.env.BULK_ERROR_OUTPUT;
 
-if(!(process.argv[2] && sid && auth && lookupType && bulkCSVHeaders)) {
+if (!(process.argv[2] && sid && auth && lookupType && bulkCSVHeaders)) {
 
-	console.log("Please pass in the following variables: SID, Auth, .csv of phone numbers");
-	process.exit(1);
+    console.log("Please pass in the input filename.");
+    process.exit(1);
 
 } else {
     console.log("You are running " + lookupType + " Lookups");
@@ -24,7 +24,6 @@ var concurrency = 50;
 var request = require('request');
 var fs = require('fs');
 var async = require('async');
-var Promise = require('bluebird');
 var rp = require('request-promise');
 
 var parse = require('csv-parse');
@@ -33,19 +32,18 @@ var transform = require('stream-transform');
 var stream = fs.createWriteStream('output-' + lookupType + '.csv');
 
 var errorStream = fs.createWriteStream('errors-' + lookupType + '.csv');
-var lastErrorNumber = "";
 
 var errorList = [];
 
 var uriSuffix = (lookupType === "fraud") ? '?Type=fraud' : '?Type=carrier';
-if(lookupType === "fraud" && bulkCSVHeaders){
+if (lookupType === "fraud" && bulkCSVHeaders) {
     stream.write("phone_number,advanced_line_type,mobile_country_code,mobile_network_code,caller_name,is_ported,last_ported_type\n");
-} else if(lookupType === "carrier" && bulkCSVHeaders) {
+} else if (lookupType === "carrier" && bulkCSVHeaders) {
     stream.write("phone_number,carrier_name,carrier_type\n");
 
 }
 
-var q = async.queue(function(task,callback){
+var q = async.queue(function (task, callback) {
 
     var uri = 'https://lookups.twilio.com/v1/PhoneNumbers/' + task.phoneNumber + uriSuffix;
 
@@ -60,55 +58,55 @@ var q = async.queue(function(task,callback){
     };
 
     rp(options)
-    .then(function(response){
+        .then(function (response) {
 
 
-        if(response.carrier.name === null) {
-            console.log("error with this number: ", response.phone_number);
-        } else if(lookupType === "fraud"){
-            stream.write(response.phone_number+','+response.fraud.advanced_line_type+','+response.fraud.mobile_country_code+','+response.fraud.mobile_network_code+','+response.fraud.caller_name+','+response.fraud.is_ported+','+response.fraud.last_ported_date+'\n');
-        } else if (lookupType === "carrier") {
-            if(response.carrier.name){
-                var carrierName = response.carrier.name.replace(",","");
+            if (response.carrier.name === null) {
+                console.log("error with this number: ", response.phone_number);
+            } else if (lookupType === "fraud") {
+                stream.write(response.phone_number + ',' + response.fraud.advanced_line_type + ',' + response.fraud.mobile_country_code + ',' + response.fraud.mobile_network_code + ',' + response.fraud.caller_name + ',' + response.fraud.is_ported + ',' + response.fraud.last_ported_date + '\n');
+            } else if (lookupType === "carrier") {
+                if (response.carrier.name) {
+                    var carrierName = response.carrier.name.replace(",", "");
+                }
+                stream.write(response.phone_number + ',' + carrierName + ',' + response.carrier.type + '\n');
             }
-            stream.write(response.phone_number+','+carrierName+','+response.carrier.type+'\n');
-        }
-        callback();
+            callback();
 
-    })
-    .catch(function(err){
-        if(task)
-        if(errorOutput){
-            console.log('big error: ', err);
-        }
-        if(!task.retries){
-            task.retries=1;
-            q.push(task);
-        }
-        else if (task.retries<5){
-            task.retries++;
-            q.push(task);
-        } else {
-            errorStream.write(task.phoneNumber+"\n");
-            errorList.push([task,'Maximum retries exceeded!']);
-        }
+        })
+        .catch(function (err) {
+            if (task)
+                if (errorOutput) {
+                    console.log('big error: ', err);
+                }
+            if (!task.retries) {
+                task.retries = 1;
+                q.push(task);
+            }
+            else if (task.retries < 5) {
+                task.retries++;
+                q.push(task);
+            } else {
+                errorStream.write(task.phoneNumber + "\n");
+                errorList.push([task, 'Maximum retries exceeded!']);
+            }
 
-        callback();
-    });
+            callback();
+        });
 
-},concurrency);
+}, concurrency);
 
-q.drain = function(){
+q.drain = function () {
     console.log('All numbers looked up.');
-    if(errorList.length>0 && errorOutput){
-        console.log('Errors:\n',errorList);
+    if (errorList.length > 0 && errorOutput) {
+        console.log('Errors:\n', errorList);
     }
 
-}
+};
 
-var csvData=[];
+var csvData = [];
 fs.createReadStream(pnCsv)
     .pipe(parse({delimiter: ':'}))
-    .on('data', function(csvrow) {
-        q.push({phoneNumber: csvrow[0]});        
+    .on('data', function (csvrow) {
+        q.push({phoneNumber: csvrow[0]});
     });
